5e74179ff7dfc5832fdca096cd62d556e202e3c2
Kuba Matyka <kmatyka@ntrc.eu>
2017-03-26 15:42:48 +0200

    create url respose

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`);
 });
+
+app.get('/', (request, response) => {
+    response.send('wszystko ok');
+});
