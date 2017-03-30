a023385b3214a5ce0c9eafb88e129eed11efdf6e
Kuba Matyka <kmatyka@ntrc.eu>
2017-03-30 17:25:42 +0200

    Connect to database and save some input

--- server.js
+++ server.js
@@ -1,29 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient
-
+const MongoClient = require('mongodb').MongoClient;
+let db;
 
 const app = express();
 const port = 3000;
+
+const url ='mongodb://kmatyka:qlop01lwe@ds145790.mlab.com:45790/book';
 
 app.use(bodyParser.urlencoded({
   encode: true,
   extended: true
 }));
 
-app.listen(port, () => {
-  console.log(`Server is up and running on port ${port}`);
-});
-
-MongoClient.connect('link-to-mongodb', (err, database) => {
-  // ... start the server
+MongoClient.connect(url, (err, res) => {
+  if (err) return console.info(err);
+  db = res;
+  app.listen(port, () => {
+    console.info(`Server is up and running on port ${port}`);
+  })
 });
 
 app.get('/', (req, res) => {
-    console.log(__dirname);
+    console.info(__dirname);
     res.sendFile(__dirname + '/index.html')
 });
 
-app.post('/titles', (req, res) => {
-    console.log(req.body);
+app.post('/book', (req, res) => {
+  db.collection('books')
+    .save(req.body, (err, res) => {
+        if (err) return console.info(err);
+        console.info('saved to database');
+        res.redirect('/');
+    })
 });
