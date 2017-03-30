3e0c525abadb8059519f3f815406828664161786
Kuba Matyka <kmatyka@ntrc.eu>
2017-03-30 09:38:57 +0200

    add mongodb client

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient
+
 
 const app = express();
 const port = 3000;
@@ -9,15 +11,19 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.listen(port, () => {
+  console.log(`Server is up and running on port ${port}`);
+});
+
+MongoClient.connect('link-to-mongodb', (err, database) => {
+  // ... start the server
+});
+
 app.get('/', (req, res) => {
     console.log(__dirname);
     res.sendFile(__dirname + '/index.html')
 });
 
 app.post('/titles', (req, res) => {
-    console.log('Wait for titles');
-});
-
-app.listen(port, () => {
-  console.log(`Server is up and running on port ${port}`);
+    console.log(req.body);
 });
